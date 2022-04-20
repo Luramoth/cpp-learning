4816f0237f8650bc9616115981010e267d83b8c4
LuraMoth <fasreaperearnest@gmail.com>
2022-04-19 22:24:08 -0700

    do some stuff!

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,18 @@
-#include <iostream>
+#include </home/kayla/Programming/cpp/cpp learning/src/headers/common.hxx>
+
+using namespace std;
+
+void greet(string yes,string answer){
+	cout << "Hello, " << yes << endl << "brub" << endl << "say your name:" << endl;
+	getline(cin,answer);
+	cout << "Hello, " << answer << endl;
+}
 
 int main(int, char**) {
-	std::cout << "Hello, world!\n";
+	string yes = "World";
+	string answer;
+
+	greet(yes,answer);
 
 	return 0;
 }
